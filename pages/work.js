13feecb56817cd46c76a696bf3a74f87f56f4d0c
k-michael-wwg <k.michael@wwg.it>
2024-08-06 17:00:07 +0300

    Portfolio Grid added and modified

--- pages/work.js
+++ pages/work.js
@@ -3,8 +3,10 @@ import { WorkGridItem } from '../components/grid-item'
 import Section from '../components/section'
 import Layout from '../components/layouts/article'
 
-import thumbExample from '../public/images/test.jpg'
-
+import thumbMagazines from '../public/images/Magazine1.jpg'
+import thumbCards from '../public/images/Card1.jpg'
+import thumbPostcards from '../public/images/Postcard1.jpg'
+import thumbBillboards from '../public/images/Board1.jpg'
 const Work = () => (
   <Layout title="Work">
     <Container>
@@ -14,9 +16,32 @@ const Work = () => (
 
       <SimpleGrid columns={[1, 1, 2]} gap={6}>
         <Section>
-          <WorkGridItem id="dialog" title="Діялог" thumbnail={thumbExample}>
-            Example Desc
-          </WorkGridItem>
+          <WorkGridItem
+            id="businesscard"
+            title="Business Cards"
+            thumbnail={thumbCards}
+          ></WorkGridItem>
+        </Section>
+        <Section>
+          <WorkGridItem
+            id="magazines"
+            title="Magazines"
+            thumbnail={thumbMagazines}
+          ></WorkGridItem>
+        </Section>
+        <Section>
+          <WorkGridItem
+            id="postcards"
+            title="Post Cards"
+            thumbnail={thumbPostcards}
+          ></WorkGridItem>
+        </Section>
+        <Section>
+          <WorkGridItem
+            id="billboards"
+            title="Billboards"
+            thumbnail={thumbBillboards}
+          ></WorkGridItem>
         </Section>
       </SimpleGrid>
     </Container>
